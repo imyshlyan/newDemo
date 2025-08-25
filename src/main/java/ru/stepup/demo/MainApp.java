8c5d004c0efc92ea0bf50e02c59792b04407b4e7
Ilya Myshlyanov <imyshlyan@gmail.com>
2025-08-25 09:10:39 +0300

    изменена логика

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -7,6 +7,7 @@ public class MainApp {
         System.out.println(1);
         System.out.println(1);
         System.out.println(2);
+        System.out.println(3);
         //TODO сменить тип на enum
     }
 
