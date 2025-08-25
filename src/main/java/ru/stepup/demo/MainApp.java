8b08ecdecc84cc1aee377475f297944703a19002
Ilya Myshlyanov <imyshlyan@gmail.com>
2025-08-25 09:15:30 +0300

    улучшили логику Main

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -5,9 +5,10 @@ public class MainApp {
         System.out.println("Hello world");
         System.out.println(1);
         System.out.println(1);
+        System.out.println("----");
         System.out.println(1);
         System.out.println(2);
-        System.out.println(3);
+        System.out.println(5);
         //TODO сменить тип на enum
     }
 
