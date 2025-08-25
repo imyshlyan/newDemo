4eddb9c8ee124c19f070e91cd6cc385e947df19d
Ilya Myshlyanov <imyshlyan@gmail.com>
2025-08-25 09:18:40 +0300

    улучшили логику Main и изменили

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -3,11 +3,11 @@ package ru.stepup.demo;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println(1);
-        System.out.println(1);
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(5);
+        System.out.println(5);
+        System.out.println(5);
+        System.out.println(5);
+        System.out.println(5);
         //TODO сменить тип на enum
     }
 
