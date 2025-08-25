03e30ef0f77385dca2f4e5d289298dd5c2a94f62
Ilya Myshlyanov <imyshlyan@gmail.com>
2025-08-25 09:04:30 +0300

    доработка логики проекта

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -5,6 +5,8 @@ public class MainApp {
         System.out.println("Hello world");
         System.out.println(1);
         System.out.println(1);
+        System.out.println(1);
+        System.out.println(2);
         //TODO сменить тип на enum
     }
 
