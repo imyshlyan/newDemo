2c6a518cb0e10f362197f0592fde1d32e5274329
Ilya Myshlyanov <imyshlyan@gmail.com>
2025-08-25 08:56:12 +0300

    initianal commit

---
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -0,0 +1,11 @@
+package ru.stepup.demo;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(1);
+        System.out.println(1);
+        //TODO сменить тип на enum
+    }
+
+}
